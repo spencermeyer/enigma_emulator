9fb4fe84aa9bb20b019fc72c2856336c7ca35b57
spencermeyer <spencer_meyer@hotmail.com>
2017-09-09 14:58:14 +0100

    got some components working and inheriting  :)

--- app/assets/javascripts/components/machine.jsx
+++ app/assets/javascripts/components/machine.jsx
@@ -1,42 +1,64 @@
-// $(function() {
-//   React.renderComponent(
-//     <div classname = "container">
-//       <p>my first jsx component</p>
-//     </div>,
-//     document.getElementById('inject-here')
-//   );
-// })
-
-// Create a component named MessageComponent
-// var Machine = React.createClass({
-//   getInitialState: function() {
-//   },
-//   render: function() {
-//     return (
-//       <div>{this.props.message}</div>
-//     );
-//   }
-// });
-
-// console.log('awooga', Machine);
-
-// // Render an instance of MessageComponent into document.body
-// React.render(
-//   <Machine message="Hello!" />,
-//   document.getElementsById('inject-here')
-// );
-
-
 class Machine extends React.Component {
   render() {
-    return <h1>{this.props.title}</h1>    
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <Rotor letter="b" />
+        <DisplayBoard />
+        <KeyBoard />
+      </div>
+    )   
   }
 }
 
+class Rotor extends React.Component{
+  render() {
+    return(
+      <p>RotorX letter {this.props.letter} </p>
+    );
+  }  
+};
 
+class DisplayBoard extends React.Component{
+  render() {
+    const displayBoardLetters = ['Q','W','E','R','T','Z','U','I','O','A','S','D','F','G','H','J','K','P','Y','X','C','V','B','N','M','L'];
+    return(
+        <div className="display-board">
+          { displayBoardLetters.map(function(individualLetter){
+            return <DisplayBoardLetter letter={individualLetter} />
+          }) }
+        </div>
+      );
+  }
+}
 
+const DisplayBoardLetter = React.createClass({
+  render: function(){
+    return (
+        <div className="lamp-board-letter">
+          <button type="button" className={"key-button" + " " + this.props.letter}>{ this.props.letter }</button>
+        </div>
+      )
+  }
+})
 
+const KeyBoard = React.createClass({
+  render: function(){
+    return (
+      <div className = 'keyboard'>
+      </div>
+      )
+  }
+})
 
+const KeyBoardButton = React.createClass({
+  render: function(){
+    return (
+        <div className="key-board-letter">
 
+        </div>
+      )
+  }
+})
 
-
+// try this    eslint app/assets/javascritps/components/machine.jsx
